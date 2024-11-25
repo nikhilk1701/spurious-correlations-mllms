81be3b4b754674d19d2243876b2ea8f1a6e640e5
Nikhil Kommineni <nk3853@ga002.hpc.nyu.edu>
2024-11-24 23:25:33 -0500

    make changes to tsne plotting code

--- Codes/Baselines/tsne_experiments.py
+++ Codes/Baselines/tsne_experiments.py
@@ -16,7 +16,7 @@ from eval_clip import *
 # from waterbird_questions import generate_questions
 # from llava_inference import llava_inference
 def load_model(model, device, load_path):
-    model_dict = torch.load(load_path)
+    model_dict = torch.load(load_path, map_location=torch.device(device))
     model.load_state_dict(model_dict['model']['state_dict'])
     model = model.to(device)
     return model
@@ -37,11 +37,11 @@ def main():
 
     device = "cuda" if torch.cuda.is_available() else "cpu"
 
-    model, preprocess = clip.load(model_type, device="cuda")
-    model = load_model(model, "/scratch/nk3853/pipeline_results0000/Run0023/Train/best.tar", device)
+    model, preprocess = clip.load(model_type, device=device)
+    model = load_model(model, device, "/scratch/nk3853/pipeline_results0000/Run0023/Train/best.tar")
     test_data = get_organized_dataset(scratch_dir + "/datasets", 'waterbirds', "test")
     dataset = CLIPDataloader(clip_transform= preprocess, clip_tokenizer=clip.tokenize, learning_data= test_data)
-    dataloader = torch.utils.data.DataLoader(dataset, batch_size=32, pin_memory=True, num_workers=2, shuffle=True)
+    dataloader = torch.utils.data.DataLoader(dataset, batch_size=32, pin_memory=True, num_workers=1, shuffle=True)
     encodings = []
     label_to_num = {
         'waterbirdwater': 0,
@@ -49,6 +49,8 @@ def main():
         'landbirdwater': 2,
         'landbirdland': 3
     }
+    num_to_label = { 0: 'waterbird', 1: 'landbird'}
+    num_to_label_4c = {v: k for k, v in label_to_num.items()}  # Reverse mapping for legend
     # for datapoint in tqdm(dataset):
     #     # object_text = datapoint['object_text']
     #     # bground_text = datapoint['bground_text']
@@ -66,22 +68,31 @@ def main():
     #     }
 
     #     encodings.append(encoding)
-    # np.save(f'{run_dir}/image_encodings-small-all-test.npy', encodings)
+    # np.save(f'{run_dir}/image_encodings-small-finetuned23-all-test.npy', encodings)
     encodings = np.load(f'{run_dir}/image_encodings-small-finetuned-all-test.npy', allow_pickle=True)
     data = np.concatenate([encoding['image'] for encoding in encodings], axis=0)
-    labels = [encoding['class'] for encoding in encodings]
+    labels = [encoding['label'] for encoding in encodings]
 
     pca = PCA(n_components=50, random_state=42)
     data_pca = pca.fit_transform(data)
 
     tsne = TSNE(n_components=2, random_state=42, perplexity=30, n_iter=1000)
     data_2d = tsne.fit_transform(data_pca)
-    scatter = plt.scatter(data_2d[:, 0], data_2d[:, 1], c=labels, cmap='viridis', alpha=0.7, s=5)
-    plt.colorbar(scatter, label='Labels')
+
+    plt.figure(figsize=(10, 8))
+    scatter_plots = []
+    for label in np.unique(labels):
+        indices = np.array(labels) == label
+        scatter = plt.scatter(data_2d[indices, 0], data_2d[indices, 1], label=num_to_label_4c[label], alpha=0.7, s=5)
+        scatter_plots.append(scatter)
+
+    # scatter = plt.scatter(data_2d[:, 0], data_2d[:, 1], c=labels, cmap='viridis', alpha=0.7, s=5)
+    plt.legend(title='Classes', loc='best')
+    # plt.colorbar(scatter, label='Labels')
     plt.title('t-SNE Visualization of 512-Dimensional Vectors')
     plt.xlabel('t-SNE Dimension 1')
     plt.ylabel('t-SNE Dimension 2')
     plt.grid(True)
-    plt.savefig("image_pretrained-clip-small-all-test2.png")
+    plt.savefig("image_finetuned23-clip-small-all-test_4class.png")
 
 main()
